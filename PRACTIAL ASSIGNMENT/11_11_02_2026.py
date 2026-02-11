f2293fa186c658dfa8222a533f4f684eb099d169
Students <student@mefgi.com>
2026-02-11 15:55:25 +0530

    11_02_2026

---
+++ PRACTIAL ASSIGNMENT/11_11_02_2026.py
@@ -0,0 +1,15 @@
+def calculate_total(*args):
+    total = 0
+    for num in args:
+        total += num
+    return total
+
+b = []
+print("Enter numbers (Type 0 to stop):")
+while True:
+    a = input("Enter a number: ")
+    if a == "0":
+        break
+    else:
+        b.append(int(a))
+print("The sum of all numbers is : ", calculate_total(*b))
